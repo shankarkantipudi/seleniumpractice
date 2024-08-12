67df6ac8a2031dc5ef2361b954e6c7f3e0264b93
shankarkantipudi <shankarkantipudi.sk666@gmail.com>
2024-08-12 21:24:33 +0530

    xpath

--- practice/src/main/java/locators/Xpath.java
+++ practice/src/main/java/locators/Xpath.java
@@ -1,9 +1,23 @@
 package locators;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Xpath {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		// maximize
+		driver.manage().window().maximize();
+		String url = "https://demoqa.com/automation-practice-form";
+		driver.get(url);
+		Thread.sleep(2000);
+		// xpath with tag name
+		WebElement email = driver.findElement(By.xpath("//input[@class='mr-sm-2 form-control']"));
+		email.sendKeys("shankar.kantipudi@gmail.com");
 
 	}
 
