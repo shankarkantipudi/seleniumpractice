28b6bbb5f310b12b4a8dc8c135df50e03c1825f1
shankarkantipudi <shankarkantipudi.sk666@gmail.com>
2024-10-09 17:10:42 +0530

    final bootstrap commit

--- selenium_practice/src/main/java/dropdown/BootstrapDropDown.java
+++ selenium_practice/src/main/java/dropdown/BootstrapDropDown.java
@@ -1,6 +1,7 @@
 package dropdown;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,9 +16,26 @@ public class BootstrapDropDown {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.get(url);
-		WebElement btdd=driver.findElement(By.xpath(""));
+		WebElement btdd=driver.findElement(By.xpath("//span[@class='multiselect-selected-text']"));
 		btdd.click();
-		
+		//select single option
+		WebElement ele=driver.findElement(By.xpath("//input[@value='Java']"));
+		ele.click();
+		//capture all the options and then find out the size
+		List<WebElement>ddoptions=driver.findElements(By.xpath("//ul[@class='multiselect-container dropdown-menu']//label"));
+		for (WebElement ddoption : ddoptions) {
+			System.out.println(ddoption.getSize());
+			System.out.println(ddoption.getText());
+			
+		}
+		//select multiple option from dropdown
+		for (WebElement op : ddoptions) {
+			if(op.getText().equalsIgnoreCase("Java") || op.getText().equalsIgnoreCase("Python")) {
+				op.click();
+			}
+			
+		}
+	
 		
 	}
 }
