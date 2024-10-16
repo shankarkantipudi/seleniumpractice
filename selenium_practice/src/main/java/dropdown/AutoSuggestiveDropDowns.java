4434a1cc3fd19c33d430026d0133b47475bb9e7b
shankarkantipudi <shankarkantipudi.sk666@gmail.com>
2024-10-16 10:32:19 +0530

    autosuggestivedropdown

---
+++ selenium_practice/src/main/java/dropdown/AutoSuggestiveDropDowns.java
@@ -0,0 +1,33 @@
+package dropdown;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class AutoSuggestiveDropDowns {
+	static WebDriver driver = new ChromeDriver();
+	static String url = "https://www.google.com/";
+
+	public static void autosuggestivedropdown() {
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get(url);
+		WebElement searchBox = driver.findElement(By.name("q"));
+		searchBox.sendKeys("MumbaiIndians");
+		List<WebElement> options=driver.findElements(By.xpath("//div[@class='wM6W7d' and @role='presentation']"));
+		int count=options.size();
+		System.out.println(count);
+		for(WebElement option:options) {
+			System.out.println(option.getText());
+			
+		}
+
+	}
+
+}
